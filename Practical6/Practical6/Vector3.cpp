0573950f6f6f3fefe2d7bd61caf6305d019a8267
Jack Fennell <C00220386@itcarlow.ie>
2018-01-11 12:53:55 +0000

    edit

--- Practical6/Practical6/Vector3.cpp
+++ Practical6/Practical6/Vector3.cpp
@@ -1,1 +1,122 @@
 #include "Vector3.h"
+
+double Vector3::getX()
+{
+	return x;
+}
+
+void Vector3::setX(double value)
+{
+	x = value;
+}
+
+double Vector3::getY()
+{
+	return y;
+}
+
+void Vector3::setY(double value)
+{
+	y = value;
+}
+
+double Vector3::getZ()
+{
+	return z;
+}
+
+void Vector3::setZ(double value)
+{
+	z = value;
+}
+
+Vector3::Vector3()
+{
+	x = 0.0f;
+	y = 0.0f;
+	z = 0.0f;
+}
+
+Vector3::Vector3(double x1, double y1, double z1)
+{
+	x = x1;
+	y = y1;
+	z = z1;
+}
+
+Vector3::Vector3(Vector3 * V)
+{
+	x = V->x;
+	y = V->y;
+	z = V->z;
+
+}
+
+double Vector3::length()
+{
+	return (double)sqrt(x * x + y * y + z * z);
+}
+
+double Vector3::lengthSquared()
+{
+	// A method to return the length squared of the vector
+	return (x * x + y * y + z * z);
+}
+
+void Vector3::normalise()
+{
+	// A method to reduce the length of the vector to 1.0 
+	// keeping the direction the same
+	if (length() > 0.0)
+	{  // Check for divide by zero
+		double magnit = length();
+		x /= magnit;
+		y /= magnit;
+		z /= magnit;
+	}
+}
+
+Vector3 Vector3::operator+(Vector3 V1)
+{
+	return Vector3();
+}
+
+Vector3 Vector3::operator-(Vector3 V)
+{
+	return Vector3();
+}
+
+Vector3 Vector3::operator-()
+{
+	return Vector3();
+}
+
+double Vector3::operator*(Vector3 V1)
+{
+	return 0.0;
+}
+
+Vector3 Vector3::operator*(double k)
+{
+	return Vector3();
+}
+
+Vector3 Vector3::operator*(float k)
+{
+	return Vector3();
+}
+
+Vector3 Vector3::operator*(int k)
+{
+	return Vector3();
+}
+
+Vector3 Vector3::operator^(Vector3 V1)
+{
+	return Vector3();
+}
+
+std::string Vector3::toString()
+{
+	return std::string();
+}
--- Practical6/Practical6/Vector3.h
+++ Practical6/Practical6/Vector3.h
@@ -1,124 +1,39 @@
 #pragma once
+
+#include <math.h>
+#include "string"
+
 class Vector3
 {
-	// The class has three variables x, y and z 
+public:
+	double getX();
+	void setX(double value);
+
+	double getY();
+	void setY(double value);
+	
+	double getZ();
+	void setZ(double value);
+
+	Vector3();
+	Vector3(double x1, double y1, double z1);
+	Vector3(Vector3 * V);
+	double length();
+	double lengthSquared();
+	void normalise();
+
+	Vector3 operator +(Vector3 V1);
+	Vector3 operator -(Vector3 V1);
+	Vector3 operator -();
+	double operator *(Vector3 V1);
+	Vector3 operator *(double k);
+	Vector3 operator *(float k);
+	Vector3 operator *(int k);
+	Vector3 operator ^(Vector3 V1);
+	std::string Vector3::toString();
+	
+private:
 	double x;
 	double y;
 	double z;
-
-public:
-	double X;
-	{
-		get{ return x; }
-	set{ x = value; }
-	}
-
-	double Y;
-	{
-		get{ return y; }
-	set{ y = value; }
-	}
-
-	double Z;
-	{
-		get{ return z; }
-	set{ z = value; }
-	}
-
-		// Constructor 1
-	 Vector3()
-	{
-		x = 0.0f;
-		y = 0.0f;
-		z = 0.0f;
-	}
-
-	// Constructor 2
-	Vector3(double x1, double y1, double z1)
-	{ // To allow other values for X, Y and Z to be declared
-		x = x1;
-		y = y1;
-		z = z1;
-	}
-	// Constructor 3
-	Vector3(Vector3 V)
-	{  // To allow other values for X, Y and Z to be declared
-		x = V.x;
-		y = V.y;
-		z = V.z;
-	}
-
-	double Length()
-	{  // A method to return the length of the vector
-		return (double)Math.Sqrt(x * x + y * y + z * z);
-	}
-	double LengthSquared()
-	{  // A method to return the length squared of the vector
-		return (this.x * this.x + this.y * this.y + this.z * this.z);
-	}
-
-	void Normalise()
-	{  // A method to reduce the length of the vector to 1.0 
-	   // keeping the direction the same
-		if (this.Length() > 0.0)
-		{  // Check for divide by zero
-			double magnit = this.Length();
-			this.x /= magnit;
-			this.y /= magnit;
-			this.z /= magnit;
-		}
-	}
-
-
-
-
-	static Vector3 operator +(Vector3 V1, Vector3 V2)
-	{  // An overloaded operator + to return the sum of 2 vectors
-		return new Vector3(V1.x + V2.x, V1.y + V2.y, V1.z + V2.z);
-	}
-
-	static Vector3 operator -(Vector3 V1, Vector3 V2)
-	{ // An overloaded operator - to return the difference of 2 vectors
-		return new Vector3(V1.x - V2.x, V1.y - V2.y, V1.z - V2.z);
-	}
-
-	static Vector3 operator -(Vector3 V)
-	{// An overloaded operator - to return the negation of a single vector
-		Vector3 V1 = new Vector3();
-		V1.x = -V.x;
-		V1.y = -V.y;
-		V1.z = -V.z;
-		return V1;
-	}
-
-	static double operator *(Vector3 V1, Vector3 V2)
-	{// An overloaded operator * to return the scalar product of 2 vectors
-		return (V1.x * V2.x + V1.y * V2.y + V1.z * V2.z);
-	}
-
-	static Vector3 operator *(double k, Vector3 V1)
-	{// An overloaded operator * to return the product of a scalar by a vector
-		return new Vector3(V1.x * (float)k, V1.y * (float)k, V1.z * (float)k);
-	}
-
-	static Vector3 operator *(float k, Vector3 V1)
-	{// An overloaded operator * to return the product of a scalar by a vector
-		return new Vector3(V1.x * k, V1.y * k, V1.z * k);
-	}
-
-	static Vector3 operator *(int k, Vector3 V1)
-	{// An overloaded operator * to return the product of a scalar by a vector
-		return new Vector3(V1.x * k, V1.y * k, V1.z * k);
-	}
-
-	static Vector3 operator ^(Vector3 V1, Vector3 V2)
-	{// An overloaded operator ^ to return the vector product of 2 vectors
-		return new Vector3(V1.y * V2.z - V1.z * V2.y, V1.z * V2.x - V1.x * V2.z, V1.x * V2.y - V1.y * V2.x);
-	}
-
-	override string ToString()
-	{
-		return "(" + x.ToString("g3") + "," + y.ToString("g3") + "," + z.ToString("g3") + ")";
-	}
-
-}
+};
